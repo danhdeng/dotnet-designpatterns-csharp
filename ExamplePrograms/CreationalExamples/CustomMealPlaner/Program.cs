d63a03b72e7f8c3f24f90aaf574e5c929fb4d0a7
danhdeng <hui_danny_deng@hotmail.com>
2022-03-02 18:34:02 -0500

    update CustomMealPlaner program

--- ExamplePrograms/CreationalExamples/CustomMealPlaner/Program.cs
+++ ExamplePrograms/CreationalExamples/CustomMealPlaner/Program.cs
@@ -6,6 +6,12 @@ namespace CustomMealPlaner;
 internal class Program { 
     private static async Task Main(string[] vs) { 
         var logger=new ConsoleLogger();
+        logger.LogInfo("Please enter customer email.");
+        var customerEmail = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(customerEmail)) {
+            logger.LogInfo("error reading customer email");
+            return;
+        }
     }        
     
 }

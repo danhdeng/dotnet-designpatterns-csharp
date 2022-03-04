e64e12f7730c091343bf4162078eb524066fe809
danhdeng <hui_danny_deng@hotmail.com>
2022-03-03 22:15:18 -0500

    added Singleton Design Pattern and example program

--- ExamplePrograms/CreationalExamples/IngredientsDatabaseClient/Program.cs
+++ ExamplePrograms/CreationalExamples/IngredientsDatabaseClient/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using CreationalPatterns.Singleton;
+
+namespace IngredientsDatabaseClient;
+
+internal class Program {
+    private const string EastClientId = "US-East";
+    private const string WestClientId = "US-West";
+    private const string NorthClientId = "US-Noth";
+    private const string SouthClientId = "US-South";
+
+    private static async Task Main(string[] args) {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("...Connecting to ingredients database....");
+        var eastClient = IngredientsDbConnectionPool.Instance;
+        var westClient = IngredientsDbConnectionPool.Instance;
+        var southClient = IngredientsDbConnectionPool.Instance;
+        var northClient = IngredientsDbConnectionPool.Instance;
+
+        //open and close connections against the ConnectinPool
+
+        await eastClient.Connect(EastClientId);
+        await westClient.Connect(WestClientId);
+        await southClient.Connect(SouthClientId);
+        await northClient.Connect(NorthClientId);
+
+        await northClient.Disconnect();
+        await southClient.Connect(SouthClientId);
+
+        await eastClient.Disconnect();
+        await westClient.Disconnect();
+        await southClient.Disconnect();
+        await northClient.Disconnect();
+
+        Console.WriteLine("...... Sesesion complete!");
+    }
+}

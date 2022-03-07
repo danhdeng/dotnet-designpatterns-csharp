062f1a8f34119913f6beb4db740574b197c70912
danhdeng <hui_danny_deng@hotmail.com>
2022-03-06 21:05:57 -0500

    add ChainOfResponsiblity Design Pattern example program

---
+++ BehavioralPatterns/ChainOfResponsibility/Handlers/ShippingLabelPrinter.cs
@@ -0,0 +1,34 @@
+
+using BehavioralPatterns.ChainOfResponsibility.Constants;
+using BehavioralPatterns.ChainOfResponsibility.Models;
+using RealisticDependencies;
+using RealisticDependencies.Logger;
+
+namespace BehavioralPatterns.ChainOfResponsibility.Handlers;
+
+    public class ShippingLabelPrinter : AbstractStep
+    {
+        private readonly IApplicationLogger _logger;
+
+        public ShippingLabelPrinter(IApplicationLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public override KombuchaSale? Handle(KombuchaSale request)
+        {
+            if (request.SaleType == SaleType.InHouse)
+            {
+                return base.Handle(request);
+            }
+
+            if (request.SaleType == SaleType.Online)
+            {
+                _logger.LogInfo("Printing shipping label for online order.", ConsoleColor.Green);
+                return base.Handle(request);
+            }
+
+            return base.Handle(request);
+        }
+    }
+
--- ExamplePrograms/BehavioralExamples/KombuchaOrderProcessor/Program.cs
+++ ExamplePrograms/BehavioralExamples/KombuchaOrderProcessor/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using BehavioralPatterns.ChainOfResponsibility.Constants;
+using BehavioralPatterns.ChainOfResponsibility.Handlers;
+using BehavioralPatterns.ChainOfResponsibility.Models;
+using RealisticDependencies.Logger;
+using RealisticDependencies.Provider;
+
+namespace KombuchaOrderProcessor;
+
+internal class Program {
+    private static void Main() {
+        var logger =new ConsoleLogger();
+
+        logger.LogInfo(" Welcome to the Kombucha Checkout System!");
+
+        var emailer = new Emailer(logger);
+
+        var cartonizer = new Cartonizer(logger);
+        var customerLoyalty =new CustomerLoyaltyHandler(logger);
+        var receiptPrinter = new ReceiptPrinter(emailer,logger);
+        var shippingLabeler = new ShippingLabelPrinter(logger);
+
+        cartonizer
+            .SetNext(customerLoyalty)
+            .SetNext(receiptPrinter)
+            .SetNext(shippingLabeler);
+
+        logger.LogInfo("Process: Cartonize > Loyalty Program > Shipping Labels > Receipt", ConsoleColor.Blue);
+
+        logger.LogInfo("-----------------------------------------------------------------", ConsoleColor.DarkBlue);
+
+        var request = new KombuchaSale();
+
+        logger.LogInfo("Are you a reward member? (y/n)", ConsoleColor.Blue);
+
+        var isRewardsMember = Console.ReadLine();
+        if (isRewardsMember != null && isRewardsMember.ToLower() == "y")
+        {
+            request.CustomerType = CustomerType.RewardsMember;
+        }
+        else {
+            request.CustomerType = CustomerType.WalkIn;
+        }
+
+        logger.LogInfo("Is this an online order? (y/n)", ConsoleColor.Blue);
+        var isOnlineOrder=Console.ReadLine();
+        if (isOnlineOrder != null && isOnlineOrder.ToLower() == "y")
+        {
+            request.SaleType = SaleType.Online;
+        }
+        else {
+            request.SaleType = SaleType.InHouse;
+        }
+
+        cartonizer.Handle(request);
+
+
+    }
+}

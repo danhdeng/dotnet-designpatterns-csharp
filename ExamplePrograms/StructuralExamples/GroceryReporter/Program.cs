6e63ab4d20119aa05aa3e29846a9faa091c8fc12
Dan Deng <hui_danny_deng@hotmail.com>
2022-03-08 15:25:59 -0500

    add Facade Design Pattern example program

--- ExamplePrograms/StructuralExamples/GroceryReporter/Program.cs
+++ ExamplePrograms/StructuralExamples/GroceryReporter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RealisticDependencies.Logger;
+using StructualPatterns.Facade;
+
+namespace ExamplPrograms.StructuralProgram.GroceryReporter;
+
+internal class Program {
+    private static void Main() {
+        var logger = new ConsoleLogger();
+        logger.LogInfo("Welcome to the Grocery Report System");
+
+        var reporter=new DailyReporter();
+
+        /// under the hood, the GroceryStoreManager is completing some 
+        /// complex business logic. 
+        /// due to this facade, we can deal with our business needs
+        /// at a higher level of abstraction than working with the 
+        /// GroceryStoreManager classes directly in this layer.
+
+        reporter.KickOffProduceReport();
+    }
+}
